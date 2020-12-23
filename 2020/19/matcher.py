6804b14300dcdafaa701ddd36fdce726e85ffaa9
Ed Hughes <ehughes@aperturecvo.com>
2020-12-23 09:04:50 -0500

    part 2

--- 2020/19/matcher.py
+++ 2020/19/matcher.py
@@ -13,7 +13,7 @@ def addRule(id, regex):
 def getRule(id):
     regex = "("
 
-#    print("ID = {}, {}".format(id, rules))
+    print("ID = {}, {}".format(id, rules[id]))
 
     for s in rules[id].split():
         if s.isalpha():
@@ -32,24 +32,26 @@ def match(ruleId, s):
     regex = getRule(ruleId)
     pattern = re.compile( regex )
 
-#    print ("matching {} against {}".format(s, regex))
+    print ("matching {} against {}".format(s, ruleId))
     return (pattern.fullmatch( s ) != None)
 
 def readRulesFile(filename):
     with open(filename) as f:
         for line in f:
-            (key, val) = line.strip().split(": ")
-            addRule(int(key), val.strip(QQ))
+            if line.strip():
+                (key, val) = line.strip().split(": ")
+                addRule(int(key), val.strip(QQ))
 
 
 def main():
-    readRulesFile("inputRules.txt")
+    readRulesFile("testinputRules.txt")
     
     matches = 0
-    with open("inputMatches.txt") as f:
+    with open("testinputMatches.txt") as f:
         for line in f:
             if match( 0, line.strip() ):
                 matches += 1
+                print ("MATCHED: {}".format(line.strip()))
 
     print("MATCHES: {}".format(matches))
 
