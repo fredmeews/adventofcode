37dbf68382a2a527edfcecbfb50f1a3ee1e184fc
Ed Hughes <ehughes@aperturecvo.com>
2020-12-23 22:54:07 -0500

    part 2

--- 2020/19/matcher.py
+++ 2020/19/matcher.py
@@ -3,16 +3,21 @@ import re
 rules = {}
 regexes = {}
 SAW = {}
+PARTIAL_MATCH = False
 QQ = "\""
 OR = "|"
+CNT=0
+
 
 def init():
+    print("init")
     rules.clear()
     regexes.clear()
     SAW = { 42 : False, 31 : False }
+    PARTIAL_MATCH = False
 
-def addRule(id, regex):
-    rules[id] = regex
+def addRule(id, rule):
+    rules[id] = rule
 
 def getRegex(id):
     if not id in regexes:
@@ -25,26 +30,54 @@ def getRegex(id):
                 regex +=  OR
             elif s.isnumeric():
                 subId = int(s)
-                regex += getRegex( subId )            
-        regex += ")"
+                if not subId == id: # avoid infinite loop
+                    regex += getRegex( subId )
+
+        # Part 2 - these special rules get a "+" regex modifier
+        regex += ")+" if id == 42 or id == 31 else ")"
+        #regex += ")"   # PART 1
+        
         regexes[id] = regex
 
 #        if (id == 8 or id == 11):
 #            print ("{}: {}".format(id, regex))
             
     return regexes[id]
-        
+
+def toBin(s):
+    return s.replace("a", "0").replace("b", "1")
 
 def match(ruleId, s):
     regex = getRegex(ruleId)
     pattern = re.compile( regex )
 
     #DEBUG
-    m = pattern.match( s )
-    if (m != None):
-        print ("ID: {} => {}".format(ruleId, m))
+    m = None
+    
+    if PARTIAL_MATCH == True:
+        #        m = pattern.search( s )
+        m = pattern.findall( s )        
+        print("\n\nID: {}, {}".format(ruleId, regex), end = " => ")
+        for tuple in m:
+            for str in tuple: 
+                if str:
+                    print(str, end=",")
+        print(" IN [{}]".format(s))
         
-    return (pattern.fullmatch( s ) != None)
+    else:
+        m = pattern.fullmatch( s )
+
+    global CNT
+#    print ("===ID: {}, INPUT: {} ".format(ruleId, CNT))
+#    CNT += 1
+    if (m == None):
+        print("\t==> NO MATCH")
+    # elif (m != None):
+    #     bin = toBin(m[0])
+    #     print ("\t==> {} => {} => {} subst in [{}]".format(int(bin, 2), bin, m[0], s ))
+    #     #        print ("ID: {} => {} subst in [{}]".format(ruleId, m, s))
+        
+    return (m != None)
 
 def readRulesFile(filename):
     with open(filename) as f:
@@ -56,10 +89,10 @@ def readRulesFile(filename):
 
 def main():
     init()
-    readRulesFile("testinputRules.txt")
+    readRulesFile("inputRules.txt")
     
     matches = 0
-    with open("testinputMatches.txt") as f:
+    with open("inputMatches.txt") as f:
         for line in f:
             if match( 0, line.strip() ):
                 matches += 1
--- 2020/19/test.py
+++ 2020/19/test.py
@@ -60,12 +60,44 @@ class TestMatch(unittest.TestCase):
         
         self.assertFalse(matcher.match(0, "bababa"))
         self.assertFalse(matcher.match(0, "aaaabbb"))        
-        self.assertFalse(matcher.match(0, "aaabbb"))        
+        self.assertFalse(matcher.match(0, "aaabbb"))
 
 
-        
-#    def test_matchReferenceRule(self):
 
+class TestPart2WTF(unittest.TestCase):
+    def setUp(self):
+        matcher.init()
+        matcher.readRulesFile("testinputRules.txt")
+
+    #start by looking at which rules ALWAYS match the same set of values and
+    # how those rules (especially rules 42 and 31)
+    # are used by the new versions of rules 8 and 11.
+    #
+    def testDebugPatterns(self):
+        matcher.PARTIAL_MATCH = True
+
+        for rule in [8,11]:
+            matcher.match(rule,"aaaaabbaabaaaaababaa")  # 9. USE THIS ONE
+
+
+    def testPartTwo(self):
+        # Override rules 8, 11 - children of root match "0"
+        # 0: 8 11
+        matcher.addRule(8, "42 | 42 8")
+        matcher.addRule(11, "42 31 | 42 11 31")
+
+        self.assertTrue (matcher.match(0, "bbabbbbaabaabba"))
+        self.assertTrue (matcher.match(0, "babbbbaabbbbbabbbbbbaabaaabaaa"))
+        self.assertTrue (matcher.match(0, "aaabbbbbbaaaabaababaabababbabaaabbababababaaa"))
+        self.assertTrue (matcher.match(0, "bbbbbbbaaaabbbbaaabbabaaa"))
+        self.assertTrue (matcher.match(0, "bbbababbbbaaaaaaaabbababaaababaabab"))
+        self.assertTrue (matcher.match(0, "ababaaaaaabaaab"))
+        self.assertTrue (matcher.match(0, "ababaaaaabbbaba"))
+        self.assertTrue (matcher.match(0, "baabbaaaabbaaaababbaababb"))
+        self.assertTrue (matcher.match(0, "abbbbabbbbaaaababbbbbbaaaababb"))
+        self.assertTrue (matcher.match(0, "aaaaabbaabaaaaababaa"))
+        self.assertTrue (matcher.match(0, "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa"))
+        self.assertTrue (matcher.match(0, "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"))
     
 if __name__ == '__main__':
     unittest.main()
