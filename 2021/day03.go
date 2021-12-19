fc431b5a70708559a4ec69715347c556f7c5fadc
Ed Hughes <ehughes@in2l.com>
2021-12-19 14:02:51 -0500

    working part 2

--- 2021/day03.go
+++ 2021/day03.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"strconv"
 	"strings"
+//	"unicode/utf8"
 )
 
 var inputFile = flag.String("inputFile", "inputs/day03.input", "Relative file path to use as input.")
@@ -17,98 +18,93 @@ type bit struct {
 
 
 // Based on length of binary string
-func getHighestBit(length int) int {
+func getHighestBit(length int) int64 {
 	s := string("1" + strings.Repeat("0", length-1))
 	hb, _ := strconv.ParseInt(s, 2, 0)
-	return int(hb)
+	return hb
 }
+
+func initializeSet(contents []string) map[int64]bool {
+	var set map[int64]bool
+	set = make(map[int64]bool)
+	
+	for _, binary := range contents {
+		i,_ := strconv.ParseInt(binary, 2, 0)
+		set[i] = true
+	}
+	return set
+}
+
+func printSet(set map[int64]bool) {
+	for k, v := range set {
+		if v == true {
+			fmt.Printf("%05b, %v",k, k )
+		}
+	}
+	fmt.Printf("\n")
+	return;
+}
+
+func getFilteredSet(set map[int64]bool, pos int64, leastCommon bool) map[int64]bool {
+	// Figure out most common in set
+	zerosMostCommon := false
+	zeros := 0
+	ones := 0
+	for k, v := range set {
+		if v == true { 
+			if (k & pos > 0) {
+				ones++
+			} else {
+				zeros++
+			}
+		}
+	}
+	if (zeros > ones) {
+		zerosMostCommon = true
+	}
+
+	// Filter set based on common
+	for k, v := range set {
+		if v == true {
+			if (k & pos == 0) {
+				set[k] = (zerosMostCommon == true)
+			} else {
+				set[k] = (zerosMostCommon == false)
+			}
+			if (leastCommon == true) {
+				set[k] = !set[k]
+			}
+		}
+		if set[k] == false {
+			delete(set, k)
+		}
+	}
+
+	return set
+}	
 
 func main() {
 	flag.Parse()
 	bytes, err := ioutil.ReadFile(*inputFile)
 	if err != nil {
 		fmt.Println(err)	
-		
 		return
 	}
 	contents := string(bytes)
 	split := strings.Split(contents, "\n")
+	highestBit := getHighestBit(len(split[0]))	
 
-	// Part 1
-	var bit map[int]int   //map[position] = count of 1s
-	bit = make(map[int]int)
-	highestBit := getHighestBit(len(split[0]))
-	
-	for _, binary := range split {
-		i,_ := strconv.ParseInt(binary, 2, 0)
-
-		for pos := highestBit; pos >= 1; pos = pos / 2 {
-			if (int(i) & pos) > 0 {
-				bit[pos]++ 	// Count the number of "1's" found in each bit
-			}
-		}
+	set := initializeSet(split)
+	for pos := highestBit; (pos >= 1 && len(set) > 1); pos = pos / 2 {
+		set = getFilteredSet(set, pos, false)
 	}
+	fmt.Println ("\n>> Most common = ")
+	printSet(set)
 
-	var gamma int = 0
-	var epsilon int = 0	
-	for pos, cnt := range bit {
-		// If most common bit is "1", set bit at this pos in final number
-		if cnt > len(split)/2 {
-			gamma = gamma | pos			
-		} else {
-			epsilon = epsilon | pos						
-		}
+	set = initializeSet(split)
+	for pos := highestBit; (pos >= 1 && len(set) > 1); pos = pos / 2 {
+		set = getFilteredSet(set, pos, true)
 	}
-
-	fmt.Println("Part1: g,e,ans", gamma, epsilon, gamma*epsilon)
-
-	fmt.Printf("Part 1 binary: %b, %b\n", gamma, epsilon)
-
-	// Part 2
-	var set map[int]bool
-	set = make(map[int]bool)
-	for pos := highestBit; pos >= 1; pos = pos / 2 {
-		mostCommon := 0
-		leastCommon := 0
-		
-		fmt.Println(">>>>", pos)
-		for _, binary := range split {
-			i,_ := strconv.ParseInt(binary, 2, 0)
-			if pos == highestBit || set[int(i)] == true {
-				if (pos & gamma) == (pos & int(i)) {
-					set[int(i)] = true
-					mostCommon++
-				} else {
-					set[int(i)] = false
-					leastCommon++
-				}
-			}
-		}
-
-		// Purge set of unwanted
-		for k, _ := range set {
-			if (mostCommon == leastCommon) { // special case - counts equal favor "1"
-				if (k & pos > 0) { 
-					fmt.Println("A", k, pos, k & pos)
-					set[k] = true
-				} else {
-					set[k] = false
-				}
-			}
-			
-			if set[k] == false {
-				fmt.Println("B", k, pos, k & pos, set[k])
-			//	delete(set, k)
-			}
-		}
-
-		for k := range set {
-			if (set[k] == true) {
-				fmt.Println("here",k, set)
-			}
-		}
-	}
+	fmt.Println ("\n>> Least common = ")
+	printSet(set)
 }
-
-
-
