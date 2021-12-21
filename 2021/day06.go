527eded87c3d74c583dbedee1919be4fa01339d2
Ed Hughes <ehughes@in2l.com>
2021-12-20 22:57:45 -0500

    day6 part 2

--- 2021/day06.go
+++ 2021/day06.go
@@ -10,10 +10,14 @@ import (
 
 var inputFile = flag.String("inputFile", "inputs/day06.input", "Relative file path to use as input.")
 
-const DAYS=80
-const RESET_TIMER=6
+const DAYS=256
+const RESET=6
 const BABY=8
 
+type Fish struct {
+	yesterday int
+	today int
+}
 
 func main() {
 	flag.Parse()
@@ -25,26 +29,41 @@ func main() {
 	contents := string(bytes)
 	split := strings.Split(contents, ",")
 
-	fish := make([]int, len(split))
-	for ix, v := range split {
-		fish[ix], _ = strconv.Atoi(v)
+	// Set Initial state
+	var fish [9]Fish
+	for _, v := range split {
+		ix,_ := strconv.Atoi(v)
+		fish[ix].today++
 	}
 
-	// Part 1
-	for i := 0; i < DAYS; i++ {
+	// Cycle through days
+//	fmt.Println("          0     1     2     3     4     5     6     7     8")
+//	fmt.Printf("Initial %v\n", fish)	
+	for i := 1; i <= DAYS; i++ {
 		fish = tick(fish)
-		fmt.Printf("Day %v:  %v\n", i, fish)
+//		fmt.Printf("Day %v:  %v\n", i, fish)
 	}
+
+	// Sum total fish
+	sum := 0
+	for i := 0; i <= 8; i++ {
+		sum += fish[i].today
+	}
+	fmt.Println("Total fish: ", sum)
 }
 
-func tick(fish []int) []int {
-	size := len(fish)
-	for i := 0; i < size; i++ {
-		fish[i]--
-		if fish[i] < 0 {
-			fish[i] = RESET_TIMER
-			fish = append(fish, BABY)
-		}
+
+func tick(fish [9]Fish) [9]Fish {
+	for i := 0; i <= 7 ; i++ {
+		fish[i].yesterday = fish[i].today // backup
+	 	fish[i].today = fish[i+1].today  // e.g. 5's yesterday are 4s today
 	}
+	
+	// reset: anything that was a zero yesterday is now a 6 ....
+	fish[RESET].today += fish[0].yesterday
+
+	// ...AND creates a baby
+	fish[BABY].today = fish[0].yesterday
+
 	return fish
 }
